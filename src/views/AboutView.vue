1d3af0acfe6d44939d05b3dd6be2583f0e3740e2
sweeterjava <sweeterjava@163.com>
2022-10-28 14:27:17 +0800

    day05

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>关于我们</h1>
+    <h1>这是关于我们页面哦，想了解我们，别错过</h1>
+    <h2>哇哈哈哈哈哈哈哈哈哈</h2>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <h1>这是主页哦，别走错了</h1>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
