9135685291d4f907097be8e655937f57b9f0317e
sweeterjava <sweeterjava@163.com>
2022-10-31 11:50:58 +0800

    day06

--- src/views/sys-admin/temp/AlbumAddNewView.vue
+++ src/views/sys-admin/temp/AlbumAddNewView.vue
@@ -9,6 +9,65 @@
 
     <el-divider></el-divider>
 
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="简介" prop="description">
+        <el-input v-model="ruleForm.description"></el-input>
+      </el-form-item>
+      <el-form-item label="排序序号" prop="sort">
+        <el-input v-model="ruleForm.sort"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">添加</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
 
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        description: '',
+        sort: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入相册名称', trigger: 'blur'},
+          {min: 4, max: 15, message: '长度在 4 到 15 个字符', trigger: 'blur'}
+        ],
+        description: [
+          {required: true, message: '请输入相册简介', trigger: 'blur'},
+          {min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur'}
+        ],
+        sort: [
+          {required: true, message: '请输入相册排序序号', trigger: 'blur'},
+          {trigger: 'blur'}
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      // 检查表单是否通过验证
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
