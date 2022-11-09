93a0e1d6acc13a1670ddfb0c7ed593b965b9570d
sweeterjava <sweeterjava@163.com>
2022-11-09 10:17:12 +0800

    day13

--- src/views/sys-admin/temp/AdminListView.vue
+++ src/views/sys-admin/temp/AdminListView.vue
@@ -13,7 +13,8 @@
       <el-table-column prop="nickname" label="昵称" width="100" align="center"></el-table-column>
       <el-table-column prop="phone" label="手机号码" width="120" align="center"></el-table-column>
       <el-table-column prop="email" label="电子邮箱" width="180" align="center"></el-table-column>
-      <el-table-column prop="description" label="简介" header-align="center" :show-overflow-tooltip="true"></el-table-column>
+      <el-table-column prop="description" label="简介" header-align="center"
+                       :show-overflow-tooltip="true"></el-table-column>
       <el-table-column label="是否启用" width="80" align="center">
         <template slot-scope="scope">
           <el-switch
