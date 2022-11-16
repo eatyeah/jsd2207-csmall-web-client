6c903aee81ff4eea8990904d1c34aa475f9d970b
sweeterjava <sweeterjava@163.com>
2022-11-16 15:34:27 +0800

    day18

---
+++ src/views/sys-admin/product/SpuAddnewStep3View.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right" style="font-size: 16px;">
+      <el-breadcrumb-item :to="{ path: '/' }">后台管理</el-breadcrumb-item>
+      <el-breadcrumb-item>新增SPU</el-breadcrumb-item>
+      <el-breadcrumb-item>第一步</el-breadcrumb-item>
+
+    </el-breadcrumb>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "SpuAddnewStep1View"
+}
+</script>
+
+<style scoped>
+
+</style>
